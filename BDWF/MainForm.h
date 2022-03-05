734dc503f6b9aef59c5536edd12d78eb034930f8
Никита Лесив <nick.lesiv@yandex.ru>
2022-03-05 14:22:51 +0300

    1

--- BDWF/MainForm.h
+++ BDWF/MainForm.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "myVector.h"
+#include "Plot.h"
 
 namespace BDWF {
 
@@ -19,11 +20,12 @@ namespace BDWF {
 		MainForm(void)
 		{
 			InitializeComponent();
-			//
-			//TODO: äîáàâüòå êîä êîíñòğóêòîğà
-			//
+
+
 
 		}
+
+
 
 	protected:
 		/// <summary>
@@ -43,12 +45,15 @@ namespace BDWF {
 	private: System::Windows::Forms::ToolStripMenuItem^ îòêğûòüToolStripMenuItem;
 	private: System::Windows::Forms::ToolStripMenuItem^ ñîõğàíèòüÂÔàéëToolStripMenuItem;
 	private: System::Windows::Forms::ToolStripMenuItem^ âûõîäToolStripMenuItem;
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::TextBox^ textBox1;
 
 	private:
 		/// <summary>
 		/// Îáÿçàòåëüíàÿ ïåğåìåííàÿ êîíñòğóêòîğà.
 		/// </summary>
-		System::ComponentModel::Container ^components;
+		System::ComponentModel::Container^ components;
 
 #pragma region Windows Form Designer generated code
 		/// <summary>
@@ -64,6 +69,9 @@ namespace BDWF {
 			this->ñîõğàíèòüÂÔàéëToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
 			this->âûõîäToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
 			this->ñïğàâêàToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
 			this->main_menuStrip->SuspendLayout();
 			this->SuspendLayout();
 			// 
@@ -76,7 +84,7 @@ namespace BDWF {
 			});
 			this->main_menuStrip->Location = System::Drawing::Point(0, 0);
 			this->main_menuStrip->Name = L"main_menuStrip";
-			this->main_menuStrip->Size = System::Drawing::Size(271, 28);
+			this->main_menuStrip->Size = System::Drawing::Size(365, 28);
 			this->main_menuStrip->TabIndex = 0;
 			this->main_menuStrip->Text = L"main_menuStrip";
 			// 
@@ -93,25 +101,25 @@ namespace BDWF {
 			// ñîçäàòüToolStripMenuItem
 			// 
 			this->ñîçäàòüToolStripMenuItem->Name = L"ñîçäàòüToolStripMenuItem";
-			this->ñîçäàòüToolStripMenuItem->Size = System::Drawing::Size(224, 26);
+			this->ñîçäàòüToolStripMenuItem->Size = System::Drawing::Size(217, 26);
 			this->ñîçäàòüToolStripMenuItem->Text = L"Ñîçäàòü";
 			// 
 			// îòêğûòüToolStripMenuItem
 			// 
 			this->îòêğûòüToolStripMenuItem->Name = L"îòêğûòüToolStripMenuItem";
-			this->îòêğûòüToolStripMenuItem->Size = System::Drawing::Size(224, 26);
+			this->îòêğûòüToolStripMenuItem->Size = System::Drawing::Size(217, 26);
 			this->îòêğûòüToolStripMenuItem->Text = L"Îòêğûòü";
 			// 
 			// ñîõğàíèòüÂÔàéëToolStripMenuItem
 			// 
 			this->ñîõğàíèòüÂÔàéëToolStripMenuItem->Name = L"ñîõğàíèòüÂÔàéëToolStripMenuItem";
-			this->ñîõğàíèòüÂÔàéëToolStripMenuItem->Size = System::Drawing::Size(224, 26);
+			this->ñîõğàíèòüÂÔàéëToolStripMenuItem->Size = System::Drawing::Size(217, 26);
 			this->ñîõğàíèòüÂÔàéëToolStripMenuItem->Text = L"Ñîõğàíèòü â ôàéë";
 			// 
 			// âûõîäToolStripMenuItem
 			// 
 			this->âûõîäToolStripMenuItem->Name = L"âûõîäToolStripMenuItem";
-			this->âûõîäToolStripMenuItem->Size = System::Drawing::Size(224, 26);
+			this->âûõîäToolStripMenuItem->Size = System::Drawing::Size(217, 26);
 			this->âûõîäToolStripMenuItem->Text = L"Âûõîä";
 			// 
 			// ñïğàâêàToolStripMenuItem
@@ -120,11 +128,42 @@ namespace BDWF {
 			this->ñïğàâêàToolStripMenuItem->Size = System::Drawing::Size(81, 24);
 			this->ñïğàâêàToolStripMenuItem->Text = L"Ñïğàâêà";
 			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(12, 108);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(339, 56);
+			this->button1->TabIndex = 1;
+			this->button1->Text = L"ÎÊ";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MainForm::button1_Click);
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(40, 59);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(17, 16);
+			this->label1->TabIndex = 2;
+			this->label1->Text = L"N";
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(63, 56);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(61, 22);
+			this->textBox1->TabIndex = 3;
+			this->textBox1->Text = L"100";
+			this->textBox1->TextAlign = System::Windows::Forms::HorizontalAlignment::Center;
+			// 
 			// MainForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(271, 163);
+			this->ClientSize = System::Drawing::Size(365, 186);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->button1);
 			this->Controls->Add(this->main_menuStrip);
 			this->MainMenuStrip = this->main_menuStrip;
 			this->Name = L"MainForm";
@@ -136,5 +175,12 @@ namespace BDWF {
 
 		}
 #pragma endregion
+
+		private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e)
+		{
+			Plot^ p1 = gcnew Plot();
+			p1->N = 100;
+			p1->Show();
+		}
 	};
 }
---
+++ BDWF/Plot.h
@@ -0,0 +1,97 @@
+#pragma once
+
+namespace BDWF {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// —водка дл€ Plot
+	/// </summary>
+	public ref class Plot : public System::Windows::Forms::Form
+	{
+	public:
+		Plot(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: добавьте код конструктора
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// ќсвободить все используемые ресурсы.
+		/// </summary>
+		~Plot()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::DataVisualization::Charting::Chart^ chart1;
+	protected:
+
+	private:
+		/// <summary>
+		/// ќб€зательна€ переменна€ конструктора.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// “ребуемый метод дл€ поддержки конструктора Ч не измен€йте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			System::Windows::Forms::DataVisualization::Charting::ChartArea^ chartArea1 = (gcnew System::Windows::Forms::DataVisualization::Charting::ChartArea());
+			System::Windows::Forms::DataVisualization::Charting::Legend^ legend1 = (gcnew System::Windows::Forms::DataVisualization::Charting::Legend());
+			System::Windows::Forms::DataVisualization::Charting::Series^ series1 = (gcnew System::Windows::Forms::DataVisualization::Charting::Series());
+			this->chart1 = (gcnew System::Windows::Forms::DataVisualization::Charting::Chart());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->chart1))->BeginInit();
+			this->SuspendLayout();
+			// 
+			// chart1
+			// 
+			chartArea1->Name = L"ChartArea1";
+			this->chart1->ChartAreas->Add(chartArea1);
+			legend1->Enabled = false;
+			legend1->Name = L"Legend1";
+			this->chart1->Legends->Add(legend1);
+			this->chart1->Location = System::Drawing::Point(27, 24);
+			this->chart1->Name = L"chart1";
+			series1->ChartArea = L"ChartArea1";
+			series1->ChartType = System::Windows::Forms::DataVisualization::Charting::SeriesChartType::FastPoint;
+			series1->IsVisibleInLegend = false;
+			series1->Legend = L"Legend1";
+			series1->Name = L"Series1";
+			this->chart1->Series->Add(series1);
+			this->chart1->Size = System::Drawing::Size(842, 834);
+			this->chart1->TabIndex = 0;
+			this->chart1->Text = L"chart1";
+			// 
+			// Plot
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(901, 891);
+			this->Controls->Add(this->chart1);
+			this->Name = L"Plot";
+			this->Text = L"Plot";
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->chart1))->EndInit();
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+
+	public: 
+		int N;
+
+	};
+}
