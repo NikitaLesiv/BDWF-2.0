efd53b6ff301a8954980f878f76130408cb7f39d
Никита Лесив <nick.lesiv@yandex.ru>
2023-06-16 15:27:48 +0300

    1

--- BDWF/MainForm.h
+++ BDWF/MainForm.h
@@ -49,6 +49,20 @@ namespace BDWF {
 	private: System::Windows::Forms::Button^ button1;
 	private: System::Windows::Forms::Label^ label1;
 	private: System::Windows::Forms::TextBox^ textBox1;
+	private: System::Windows::Forms::Button^ button_GenerateBeam;
+	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::GroupBox^ groupBox_beam;
+	private: System::Windows::Forms::GroupBox^ groupBox_field;
+	private: System::Windows::Forms::Button^ button3;
+	private: System::Windows::Forms::Button^ button5;
+	private: System::Windows::Forms::Button^ button4;
+
+
+
+
+
+
+
 
 	private:
 		/// <summary>
@@ -73,7 +87,16 @@ namespace BDWF {
 			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->button_GenerateBeam = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->groupBox_beam = (gcnew System::Windows::Forms::GroupBox());
+			this->groupBox_field = (gcnew System::Windows::Forms::GroupBox());
+			this->button3 = (gcnew System::Windows::Forms::Button());
+			this->button4 = (gcnew System::Windows::Forms::Button());
+			this->button5 = (gcnew System::Windows::Forms::Button());
 			this->main_menuStrip->SuspendLayout();
+			this->groupBox_beam->SuspendLayout();
+			this->groupBox_field->SuspendLayout();
 			this->SuspendLayout();
 			// 
 			// main_menuStrip
@@ -85,7 +108,7 @@ namespace BDWF {
 			});
 			this->main_menuStrip->Location = System::Drawing::Point(0, 0);
 			this->main_menuStrip->Name = L"main_menuStrip";
-			this->main_menuStrip->Size = System::Drawing::Size(365, 28);
+			this->main_menuStrip->Size = System::Drawing::Size(777, 28);
 			this->main_menuStrip->TabIndex = 0;
 			this->main_menuStrip->Text = L"main_menuStrip";
 			// 
@@ -102,25 +125,25 @@ namespace BDWF {
 			// ñîçäàòüToolStripMenuItem
 			// 
 			this->ñîçäàòüToolStripMenuItem->Name = L"ñîçäàòüToolStripMenuItem";
-			this->ñîçäàòüToolStripMenuItem->Size = System::Drawing::Size(217, 26);
+			this->ñîçäàòüToolStripMenuItem->Size = System::Drawing::Size(224, 26);
 			this->ñîçäàòüToolStripMenuItem->Text = L"Ñîçäàòü";
 			// 
 			// îòêðûòüToolStripMenuItem
 			// 
 			this->îòêðûòüToolStripMenuItem->Name = L"îòêðûòüToolStripMenuItem";
-			this->îòêðûòüToolStripMenuItem->Size = System::Drawing::Size(217, 26);
+			this->îòêðûòüToolStripMenuItem->Size = System::Drawing::Size(224, 26);
 			this->îòêðûòüToolStripMenuItem->Text = L"Îòêðûòü";
 			// 
 			// ñîõðàíèòüÂÔàéëToolStripMenuItem
 			// 
 			this->ñîõðàíèòüÂÔàéëToolStripMenuItem->Name = L"ñîõðàíèòüÂÔàéëToolStripMenuItem";
-			this->ñîõðàíèòüÂÔàéëToolStripMenuItem->Size = System::Drawing::Size(217, 26);
+			this->ñîõðàíèòüÂÔàéëToolStripMenuItem->Size = System::Drawing::Size(224, 26);
 			this->ñîõðàíèòüÂÔàéëToolStripMenuItem->Text = L"Ñîõðàíèòü â ôàéë";
 			// 
 			// âûõîäToolStripMenuItem
 			// 
 			this->âûõîäToolStripMenuItem->Name = L"âûõîäToolStripMenuItem";
-			this->âûõîäToolStripMenuItem->Size = System::Drawing::Size(217, 26);
+			this->âûõîäToolStripMenuItem->Size = System::Drawing::Size(224, 26);
 			this->âûõîäToolStripMenuItem->Text = L"Âûõîä";
 			// 
 			// ñïðàâêàToolStripMenuItem
@@ -131,9 +154,9 @@ namespace BDWF {
 			// 
 			// button1
 			// 
-			this->button1->Location = System::Drawing::Point(12, 108);
+			this->button1->Location = System::Drawing::Point(578, 46);
 			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(339, 56);
+			this->button1->Size = System::Drawing::Size(157, 56);
 			this->button1->TabIndex = 1;
 			this->button1->Text = L"ÎÊ";
 			this->button1->UseVisualStyleBackColor = true;
@@ -142,7 +165,7 @@ namespace BDWF {
 			// label1
 			// 
 			this->label1->AutoSize = true;
-			this->label1->Location = System::Drawing::Point(40, 59);
+			this->label1->Location = System::Drawing::Point(578, 105);
 			this->label1->Name = L"label1";
 			this->label1->Size = System::Drawing::Size(17, 16);
 			this->label1->TabIndex = 2;
@@ -150,18 +173,89 @@ namespace BDWF {
 			// 
 			// textBox1
 			// 
-			this->textBox1->Location = System::Drawing::Point(63, 56);
+			this->textBox1->Location = System::Drawing::Point(601, 102);
 			this->textBox1->Name = L"textBox1";
 			this->textBox1->Size = System::Drawing::Size(61, 22);
 			this->textBox1->TabIndex = 3;
 			this->textBox1->Text = L"100";
 			this->textBox1->TextAlign = System::Windows::Forms::HorizontalAlignment::Center;
 			// 
+			// button_GenerateBeam
+			// 
+			this->button_GenerateBeam->Location = System::Drawing::Point(6, 21);
+			this->button_GenerateBeam->Name = L"button_GenerateBeam";
+			this->button_GenerateBeam->Size = System::Drawing::Size(261, 39);
+			this->button_GenerateBeam->TabIndex = 4;
+			this->button_GenerateBeam->Text = L"Ãåíåðàöèÿ ïó÷êà";
+			this->button_GenerateBeam->UseVisualStyleBackColor = true;
+			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(6, 21);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(261, 39);
+			this->button2->TabIndex = 5;
+			this->button2->Text = L"Ìàãíèòíàÿ ôîêóñèðóþùàÿ ñèñòåìà";
+			this->button2->UseVisualStyleBackColor = true;
+			// 
+			// groupBox_beam
+			// 
+			this->groupBox_beam->Controls->Add(this->button_GenerateBeam);
+			this->groupBox_beam->Location = System::Drawing::Point(19, 42);
+			this->groupBox_beam->Name = L"groupBox_beam";
+			this->groupBox_beam->Size = System::Drawing::Size(273, 193);
+			this->groupBox_beam->TabIndex = 6;
+			this->groupBox_beam->TabStop = false;
+			this->groupBox_beam->Text = L"Ïó÷îê çàðÿæåííûõ ÷àñòèö";
+			// 
+			// groupBox_field
+			// 
+			this->groupBox_field->Controls->Add(this->button5);
+			this->groupBox_field->Controls->Add(this->button4);
+			this->groupBox_field->Controls->Add(this->button3);
+			this->groupBox_field->Controls->Add(this->button2);
+			this->groupBox_field->Enabled = false;
+			this->groupBox_field->Location = System::Drawing::Point(298, 42);
+			this->groupBox_field->Name = L"groupBox_field";
+			this->groupBox_field->Size = System::Drawing::Size(274, 193);
+			this->groupBox_field->TabIndex = 7;
+			this->groupBox_field->TabStop = false;
+			this->groupBox_field->Text = L"Ìîäåëèðóåìàÿ çàäà÷à";
+			// 
+			// button3
+			// 
+			this->button3->Location = System::Drawing::Point(6, 66);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(261, 32);
+			this->button3->TabIndex = 6;
+			this->button3->Text = L"×èêåéí";
+			this->button3->UseVisualStyleBackColor = true;
+			// 
+			// button4
+			// 
+			this->button4->Location = System::Drawing::Point(6, 104);
+			this->button4->Name = L"button4";
+			this->button4->Size = System::Drawing::Size(261, 31);
+			this->button4->TabIndex = 7;
+			this->button4->Text = L"Äå÷èðïåð";
+			this->button4->UseVisualStyleBackColor = true;
+			// 
+			// button5
+			// 
+			this->button5->Location = System::Drawing::Point(6, 141);
+			this->button5->Name = L"button5";
+			this->button5->Size = System::Drawing::Size(261, 43);
+			this->button5->TabIndex = 8;
+			this->button5->Text = L"Êèëüâàòåðíàÿ äèýëåêòðè÷åñêàÿ ñòðóêòóðà";
+			this->button5->UseVisualStyleBackColor = true;
+			// 
 			// MainForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(365, 186);
+			this->ClientSize = System::Drawing::Size(777, 297);
+			this->Controls->Add(this->groupBox_field);
+			this->Controls->Add(this->groupBox_beam);
 			this->Controls->Add(this->textBox1);
 			this->Controls->Add(this->label1);
 			this->Controls->Add(this->button1);
@@ -171,6 +265,8 @@ namespace BDWF {
 			this->Text = L"BDWF";
 			this->main_menuStrip->ResumeLayout(false);
 			this->main_menuStrip->PerformLayout();
+			this->groupBox_beam->ResumeLayout(false);
+			this->groupBox_field->ResumeLayout(false);
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
